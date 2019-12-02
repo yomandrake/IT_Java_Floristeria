64093c2997254e6720c322402f95a92bf61b9f7b
yomandrake <yomandrake@gmail.com>
2019-12-02 12:17:53 +0100

    Main class and start to work in user interaction

--- src/src/Floristeria/Floristeria.java
+++ src/src/Floristeria/Floristeria.java
@@ -1,5 +1,34 @@
 package src.Floristeria;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Floristeria {
 
+	private String shopName;
+//	private List<Tree> treeStock = new ArrayList<>();
+//	private List<Flower> flowerStock = new ArrayList<>();
+//	private List<Deco> decoStock = new ArrayList<>();
+	
+	
+	public String getShopName() {
+		return shopName;
+	}
+
+
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+
+
+	public Floristeria(String shopName) {
+		this.shopName = shopName;		
+	}
+	
+	
+	
+	
+	
+	
+	
 }
--- src/src/Floristeria/Main.java
+++ src/src/Floristeria/Main.java
@@ -1,10 +1,69 @@
 package src.Floristeria;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-
+	
+	public static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		List<Floristeria> myFloristerias = new ArrayList<>();
+		boolean executeMain = true;
+		char userChoice;
+		
+		do{
+			System.out.println("\nHola Bienvenido a la fabrica de floristerias: "
+					+ "\n\t 1-Crear una floristeria "
+					+ "\n\t 2-Editar floristerias creadas"
+					+ "\n\t 0-Salir de la aplicacion");
+			System.out.print("Opcion: ");
+			userChoice = sc.next().charAt(0);
+			
+			switch(userChoice) {	
+				case '1':
+					System.out.print("\t Has elegido registrar una Floristeria nueva"
+							+"\n\t Introduce el nombre = ");
+					String newShopName = sc.next();
+					myFloristerias.add(new Floristeria(newShopName));					
+					break;
 
+				case '2':
+					System.out.println("Las floristerias que existen son: ");
+					//myFloristerias.forEach(floristeria -> System.out.println(floristeria.getShopName()));
+					
+					for (int i = 0; i < myFloristerias.size(); i++) {
+						System.out.println(i + " - " + myFloristerias.get(i).getShopName());
+						
+					}
+					
+					break;
+
+				case '0':
+					executeMain = false;
+					break;
+				
+				default:
+					System.out.println("Intenta otra vez.....");
+					break;
+				
+			
+			}
+			
+		}while(executeMain);
+		
+		
+		if(userChoice == '1') {
+			
+		}else if(userChoice == '2') {
+			
+		}else {
+			
+		}
+				
+		
 	}
 
 }
